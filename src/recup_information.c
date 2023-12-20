605209705c59d36ec237d7b1a48f30be65e7ae9b
Tilio <tilio.huart@epitech.eu>
2023-12-20 13:56:24 +0100

    [UPDATE] Fix crash when we don't have a '\n' at the end of the rdr

--- src/recup_information.c
+++ src/recup_information.c
@@ -12,6 +12,12 @@ char **recup_information(char **av)
     char **array = NULL;
     char *map = read_info(av);
 
+    for (int i = 80; map[i - 1] != '\0'; i += 1) {
+        if (map[i] == '\0' && map[i - 1] != '\n') {
+            map[i] = '\n';
+            map[i + 1] = '\0';
+        }
+    }
     array = my_str_to_word_array(map);
     free(map);
     return array;
