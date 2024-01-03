43b7da9c33fc80c314a00ea69aff6130f0bda41d
Tilio <tilio.huart@epitech.eu>
2024-01-03 13:01:54 +0100

    [UPDATE] Close widow when all plane have dispawn

--- src/launcher.c
+++ src/launcher.c
@@ -7,18 +7,32 @@
 
 #include "radar.h"
 
+static void stop_radar(radar_t *radar)
+{
+    int count = 0;
+    int count2 = 0;
+
+    for (int i = 0; radar->plane[i] != NULL; i += 1)
+        count += 1;
+    for (int i = 0; radar->plane[i] != NULL; i += 1)
+        if (radar->plane[i]->disp == 1)
+            count2 += 1;
+    if (count == count2)
+        sfRenderWindow_close(radar->window);
+}
+
 int launcher(char **info)
 {
-    radar_t *radar;
+    radar_t *radar = malloc(sizeof(radar_t));
     sfClock *clock = sfClock_create();
 
-    radar = malloc(sizeof(radar_t));
     radar->info = info;
     send_information(radar);
     initialisation(radar);
     while (sfRenderWindow_isOpen(radar->window)) {
         display_radar(radar, clock);
         analyse_event(radar);
+        stop_radar(radar);
     }
     sfRenderWindow_close(radar->window);
     sfClock_destroy(clock);
